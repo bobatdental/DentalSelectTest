7c9604463689362973d52ab5798a64a55eab362c
Bob Strickland <bobstrickland.ut@gmail.com>
2018-01-03 15:27:27 -0700

    Added Subscriber container class

---
+++ DentalSelect.Web/DentalSelect.Web/BL/Subscriber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DentalSelect.Web.BL
+{
+    public class Subscriber
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(128)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(128)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string State { get; set; }
+        [Required]
+        [MaxLength(16)]
+        [Display(Name = "Zip Code")]
+        [DataType(DataType.PostalCode)]
+        [RegularExpression(@"^(?!00000)[0-9]{5,5}$", ErrorMessage = "The Zip code should contain 5 digits and should not be 00000.")]
+        public string ZipCode { get; set; }
+    }
+}
