ca299e869ac88d6bac187191c166fe42694e5edc
Bob Strickland <bobstrickland.ut@gmail.com>
2018-01-03 15:38:39 -0700

    Added database context, Subscribers Controller, set catalog = Forms...

--- DentalSelect.Web/DentalSelect.Web/DAL/Subscriber.cs
+++ DentalSelect.Web/DentalSelect.Web/DAL/Subscriber.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace DentalSelect.Web.BL
+namespace DentalSelect.Web.DAL
 {
     public class Subscriber
     {
