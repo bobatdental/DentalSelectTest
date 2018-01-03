cffb56166fa1729ed303d4578d4b678c5b98f8cd
Bob Strickland <bobstrickland.ut@gmail.com>
2018-01-03 16:23:57 -0700

    Added StateHelper for state drop down list

---
+++ DentalSelect.Web/DentalSelect.Web/BL/StateHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DentalSelect.Web.BL
+{
+    public static class StateHelper
+    {
+        public static IEnumerable<SelectListItem> StateList()
+        {
+            var items = new List<SelectListItem>
+            {
+                new SelectListItem() {Text = "Colorado", Value = "CO"},
+                new SelectListItem() {Text = "Nevada", Value = "NV"},
+                new SelectListItem() {Text = "New Mexico", Value = "NM"},
+                new SelectListItem() {Text = "Utah", Value = "UT"},
+                new SelectListItem() {Text = "Wyoming", Value = "WY"}
+            };
+            return items;
+        }
+    }
+}
