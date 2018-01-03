fbe8aa6691ec0c3a8b765ad6046371a12d4ea938
Bob Strickland <bobstrickland.ut@gmail.com>
2018-01-03 15:54:48 -0700

    Removed extra template stuff not needed and modifed default route to go to Create view

--- DentalSelect.Web/DentalSelect.Web/App_Start/RouteConfig.cs
+++ DentalSelect.Web/DentalSelect.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DentalSelect.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Subscribers", action = "Create", id = UrlParameter.Optional }
             );
         }
     }
