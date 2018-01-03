cf49b98d613b3e8720dd3f247e7e624ecc7fe802
Bob Strickland <bobstrickland.ut@gmail.com>
2018-01-03 16:00:26 -0700

    Removed Details and Edit logic since not needed for this test

--- DentalSelect.Web/DentalSelect.Web/Controllers/SubscribersController.cs
+++ DentalSelect.Web/DentalSelect.Web/Controllers/SubscribersController.cs
@@ -20,21 +20,6 @@ namespace DentalSelect.Web.Controllers
             return View(db.Subscribers.ToList());
         }
 
-        // GET: Subscribers/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Subscriber subscriber = db.Subscribers.Find(id);
-            if (subscriber == null)
-            {
-                return HttpNotFound();
-            }
-            return View(subscriber);
-        }
-
         // GET: Subscribers/Create
         public ActionResult Create()
         {
@@ -56,38 +41,7 @@ namespace DentalSelect.Web.Controllers
             }
 
             return View(subscriber);
-        }
-
-        // GET: Subscribers/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Subscriber subscriber = db.Subscribers.Find(id);
-            if (subscriber == null)
-            {
-                return HttpNotFound();
-            }
-            return View(subscriber);
-        }
-
-        // POST: Subscribers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,State,ZipCode")] Subscriber subscriber)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(subscriber).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(subscriber);
-        }
+        }     
 
         // GET: Subscribers/Delete/5
         public ActionResult Delete(int? id)
